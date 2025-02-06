894e823be5d283a4bfe3115578f657ad2f7a0dfd
Sachin-2006 <sachin06kaizen@gmail.com>
2025-02-06 17:06:34 +0530

    ignore nodemodules

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
+const app = express()
+const port = process.env.PORT
+app.use(cors())
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+
+    res.status(400).send("Hello World");
+})
+
+app.listen(port,()=>{
+    console.log("Server Connected Sucessfully =)")
+});
+
+
+
+
+
