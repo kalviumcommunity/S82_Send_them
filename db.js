e26469504b6d7e283e67ea335e0fe41aa3e01f22
Sachin-2006 <sachin06kaizen@gmail.com>
2025-02-20 13:19:21 +0530

    mongo-atlas

---
+++ db.js
@@ -0,0 +1,12 @@
+
+module.exports = async()=>{
+    try{
+    await mongoose.connect(process.env.DB_URL)
+    console.log(`mongo db connected sucessfully`)
+
+    }
+    catch(e){
+        console.log(`Went Wrong: ${e.message}`);
+        process.exit(0)
+    }
+}
