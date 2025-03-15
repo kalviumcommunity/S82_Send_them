4788abb1b0fae6d1492c6a88c8a420781509a712
Sachin-2006 <sachin06kaizen@gmail.com>
2025-03-15 10:53:43 +0530

    Woked on dummy variable

--- client/frontend/src/Components/Home.jsx
+++ client/frontend/src/Components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import MemeGen from './MemeGen'
 
 function Home() {
   return (
     <div>
+
+      
+      
       Hello World !
+      <MemeGen></MemeGen>
     </div>
   )
 }
---
+++ client/frontend/src/Components/MemeGen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MemeGen() {
+  return (
+    <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7gDLADGIeifJnQWb8E6E8tZb7oZoLdEV9KQ&s" alt="" />
+      Me when Exam
+    
+    </div>
+  )
+}
+
+export default MemeGen
